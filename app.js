bb51180368f4dcdef6f81cc102b3cb7accf7e087
Ruslan04071996 <ruslan040796@mail.ru>
2019-12-27 01:47:13 +0300

    ПЕрвый коммит

---
+++ app.js
@@ -0,0 +1,62 @@
+/*
+Задача 5
+
+Функция принимает строку с числами, раздененными пробелом
+Нужно вернуть строку с максимальным и минимальным числом
+
+Пример:
+
+highAndLow("1 2 3 4 5");  // "5 1"
+highAndLow("1 2 -3 4 5"); // "5 -3"
+highAndLow("1 9 3 4 -5"); // "9 -5"
+*/
+
+function fifth(str) {
+    //убираю лишние пробелы в строке
+    let newStr = str.replace(/\s+/gi, '');
+
+    let result = Array.from(newStr);
+    // result.filter(item => {
+    //     return item >= 0;
+    // });
+
+    result.sort((a, b) => {
+        return a - b;
+    });
+
+
+    //В этот массив будем засовывать максимальное и минимальное число
+    let lastArr = [];
+    lastArr.push(result[result.length - 1]);
+    lastArr.push(result[0]);
+
+    //Превратили массив в строку, и вернули ее
+    let resultStr = lastArr.join(' '); // Сделал пробел в строке путем добавления его методом join
+    return resultStr;
+    console.log(resultStr)
+
+
+};
+
+// console.log(fifth('1 2 3 4 5 6 '));
+// console.log( "hello world".replace(/\s/g,''))
+
+//Функция не работает с отрицательными значениями
+function fifthHomework(str) {
+    const strWithoutSpaces = str.replace(/\s+/gi, '');
+
+    let arrForSorting = Array.from(strWithoutSpaces);
+    arrForSorting.sort((a, b) => {
+        return a - b;
+    });
+
+    let arrForMinAndMaxValues = [];
+    arrForMinAndMaxValues.push(arrForSorting[arrForSorting.length - 1]);
+    arrForMinAndMaxValues.push(arrForSorting[0]);
+
+
+    let strWithMinAndMaxValues = arrForMinAndMaxValues.join(' ');
+    return strWithMinAndMaxValues;
+};
+console.log(fifthHomework('1 2 3 -5'));
+
